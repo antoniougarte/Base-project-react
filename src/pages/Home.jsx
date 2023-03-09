ed4b0af74067ba2c50c6749635ece795dbb0d5fb
antoniougarte <antoniougartemendoza@gmail.com>
2023-03-09 18:09:04 -0500

    corriendo fallos en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,12 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Products from '../components/Products'
 import Slider from '../components/Slider'
+import styled from 'styled-components'
+
+const DivContainer = styled.div`
+  background-color:red;
+  padding: 1rem 0;
+`
 
 function Home() {
   return (
